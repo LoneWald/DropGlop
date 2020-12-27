124434f08cfe84496e563f4135701cfa94c243c6
123 <bondarev.bogdan2013@yandex.ru>
2020-12-27 13:07:41 +0300

    unity

---
+++ New Unity Project (2)/Assets/Scripts/Move_Platform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Move_Platform : MonoBehaviour
+{
+    public float X = 1;
+    public float Y = 0;
+    public float Z = 0;
+    public int speed = 5;
+    public float delay = 0;
+    private double range = 0;
+    private double delay_time_up = 0, delay_time_down = 0;
+    private int Max = 0;
+    void Start()
+    {
+        range = Math.Sqrt(X * X + Y * Y + Z * Z);
+        delay_time_up = 0;
+        delay_time_down = Math.Abs(speed)- Time.deltaTime;
+        Max = 0;
+    }
+    void FixedUpdate()
+    {
+        if (delay_time_up < delay)
+            delay_time_up += Time.deltaTime;
+        else
+        {
+            delay_time_down -= Time.deltaTime;
+            transform.Translate(new Vector3(X / speed * Time.deltaTime, Y / speed * Time.deltaTime, Z / speed * Time.deltaTime));
+            Max++;
+            
+            if (Max >=Math.Abs(speed)*50)
+            {
+                speed = -speed;
+                Max = 0;
+            }
+            if (delay_time_down <= 0)
+            {
+                delay_time_up = 0;
+                delay_time_down = Math.Abs(speed) - Time.deltaTime;
+            }
+            
+        }
+    }
+}
---
+++ New Unity Project (2)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody rb;
+
+    public Vector3 startPos = new Vector3(0, 0, 0);
+    public bool collisiya;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float hoverHorizontal = Input.GetAxis("Horizontal");
+        float hoverVertcal = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hoverHorizontal, 0.0f, hoverVertcal);
+
+        rb.AddForce(movement * speed);
+
+        if(Input.GetButton("Cancel"))
+            SceneManager.LoadScene(0);
+        if (Input.GetKey("r")){
+            transform.position = startPos;
+            transform.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            rb.velocity = Vector3.zero;
+         }
+        if (collisiya == true)
+        {
+            count++;
+            if (count > 3){
+            collisiya = false; 
+            count = 0;
+            }
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "res")
+        {
+            transform.position = startPos;
+            transform.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            rb.velocity = Vector3.zero;
+            collisiya = true;
+        }
+        if (other.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
